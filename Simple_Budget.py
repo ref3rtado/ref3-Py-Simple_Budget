def9d52587a9c6b948a8754100d0f0b3fd9302c0
ref3rtado <rfurtado3.dev@gmail.com>
2025-06-05 18:13:13 -0400

    Branstorming program structure

---
+++ Simple_Budget.py
@@ -0,0 +1,12 @@
+## Main Script for Simple Budget Application ##
+#TODO: Import db_relay module
+
+#TODO: On startup, check if the database exists by talking to db_relay.py
+
+#TODO: Print a welcome message, interaction menu, and prompt for user input
+##Example: 1) Add Transaction | 2) Check Balance | 3) Modify Tables | 4) Exit
+##On "1"
+### Pull and enumerate the columns in the database, present as numbered options -- 1) Grocery, 2) Shopping, etc. 
+#### On "1"
+##### Prompt for amount, optional description, and date
+###### On enter, print something like "Transaction added" /n "Current Balance: $X.XX"
---
+++ database/db_relay.py
@@ -0,0 +1,3 @@
+#TODO: Check for existence of db_location.json file
+###### If not present, prompt user to create it, then use the specified path to create the database
+#TODO: Import tinyDB 
